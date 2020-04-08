9898d2b36ea6b7edf838d32bfb4a4b8b751736c2
Kevi Nasto <kevin.nasto@nerdery.com>
2020-04-07 19:32:56 -0500

    Added csv file that compiles all the data, Updated readme

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-A US states map polling coronavirus cases reported from John Hopkins refreshing every few hours (when data is available). The cases are divided by population of each state to get a per capita estimate. https://us-covid19-per-capita.net
+A US states map polling coronavirus cases reported from Worldometer and the JHU data and refreshing every hour (when new data is available). The cases are divided by population of each state to get a per capita estimate. https://us-covid19-per-capita.net
 
 ## Downloading the data
 
 When new data is available, it is updated in [data.csv](https://github.com/silverdrake11/covid_rates_per_capita/blob/master/data.csv). To download all snapshots of the data run `python download.py` (going back to March 21). It will get downloaded in a format for example `2020-03-23-13-04.csv` which corresponds to the date and time in UTC. The last two numbers are hours and minutes.
+
+All of snapshots are then added to one file `historical.csv`. Which looks like this
+
+| states | confirmed     | deaths | timestamp        | 
+| ------ | ------------- | ------ | ---------------- |
+| AZ     | 2575          | 73     | 2020-04-07 17:07 |
+| AZ     | 2851          | 73     | 2020-04-07 20:22 |
+| CA     | 1249          | 24     | 2020-03-21 19:20 |
--- download.py
+++ download.py
@@ -1,46 +1,109 @@
+import csv
+import json
 import os
 import time
 from datetime import datetime
+from urllib.request import urlopen
 
-import requests
 
-
-COMMITS_URL = 'https://api.github.com/repos/silverdrake11/covid_rates_per_capita/commits?page={}&path=data.csv'
+COMMITS_URL = 'https://api.github.com/repos/silverdrake11/covid_rates_per_capita/commits?page={}&path=data.csv&per_page={}'
 CONTENTS_URL = 'https://raw.githubusercontent.com/silverdrake11/covid_rates_per_capita/{}/data.csv'
 DIRNAME = 'historical'
+DATETIME_FORMAT = '%Y-%m-%d-%H-%M'
 
 
 def get_commits():
-    num_commits = 1 # Initialize
     page_num = 1
     all_commits = []
-    while num_commits:
-        response = requests.get(COMMITS_URL.format(page_num))
-        commits = response.json()
+    per_page = 100
+    num_commits = per_page # Initialize
+    while num_commits == per_page:
+        text = urlopen(COMMITS_URL.format(page_num, per_page)).read()
+        commits = json.loads(text)
         all_commits.extend(commits)
         num_commits = len(commits)
         page_num += 1
     return all_commits
 
 
-def get_file_text(sha):
-    return requests.get(CONTENTS_URL.format(sha)).text
+def get_file_bytes(sha):
+    return urlopen(CONTENTS_URL.format(sha)).read()
 
 
-commits = get_commits()
+def download_files(dirpath):
 
-if not os.path.isdir(DIRNAME):
-    os.mkdir(DIRNAME)
+    if not os.path.isdir(dirpath):
+        os.mkdir(dirpath)
 
-for commit in commits:
-    sha = commit['sha']
-    timestamp = commit['commit']['committer']['date']
-    datetime_obj = datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%SZ')
-    filename = datetime_obj.strftime('%Y-%m-%d-%H-%M.csv')
-    filepath = os.path.join(DIRNAME, filename)
-    if os.path.isfile(filepath):
-        print('{} already exists!'.format(filename))
-    else:
-        print(filepath)
-        with open(filepath, 'w') as fh:
-            fh.write(get_file_text(sha))
+    commits = get_commits()
+    for commit in commits:
+        sha = commit['sha']
+        timestamp = commit['commit']['committer']['date']
+        datetime_obj = datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%SZ')
+        filename = datetime_obj.strftime(DATETIME_FORMAT + '.csv')
+        filepath = os.path.join(dirpath, filename)
+        if os.path.isfile(filepath):
+            print('{} already exists'.format(filename))
+        else:
+            print(filepath)
+            with open(filepath, 'wb') as fh:
+                fh.write(get_file_bytes(sha))
+
+
+def read_files(dirpath):
+
+    table = {}
+
+    for filename in sorted(os.listdir(dirpath)):
+
+        if len(filename.split('-')) != 5:
+            continue
+
+        filepath = os.path.join(dirpath, filename)
+
+        with open(filepath) as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+
+            for row in csv_reader:
+
+                state = row['codes']
+                confirmed = int(float(row['confirmed']))
+                deaths = int(float(row['deaths']))
+
+                key = (state, confirmed, deaths)
+
+                if key in table:
+                    continue
+
+                dt = datetime.strptime(filename.rstrip('.csv'), DATETIME_FORMAT)
+                table[key] = dt
+    return table
+
+
+def write_final_csv(dirpath, table):
+
+    output_filepath = os.path.join(dirpath, 'historical.csv')
+    with open(output_filepath, mode='w') as fh:
+
+        fieldnames = ['state', 'confirmed', 'deaths', 'timestamp']
+        csv_writer = csv.DictWriter(fh, fieldnames=fieldnames)
+        csv_writer.writeheader()
+
+        for key in table:
+
+            (state, confirmed, deaths) = key
+            timestamp = table[key].strftime('%Y-%m-%d %H:%M')
+            row_dict = {'state': state, 
+                'confirmed':confirmed, 
+                'deaths':deaths, 
+                'timestamp': timestamp}
+            csv_writer.writerow(row_dict)
+    print()
+    print("OUTPUT: " + os.path.abspath(output_filepath))
+
+
+if __name__ == '__main__':
+    download_files(DIRNAME)
+    table = read_files(DIRNAME)
+    write_final_csv(DIRNAME, table)
+
