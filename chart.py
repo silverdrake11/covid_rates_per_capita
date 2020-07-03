57cd5ffd8b7d47ccfd7024831a425edefdeab765
Kevin Nasto <kevinnasto@gmail.com>
2020-07-03 14:17:06 -0500

    Missing data for yesterday, so using covidtracking

--- chart.py
+++ chart.py
@@ -59,9 +59,9 @@ def get_last_n(postal_code, column):
     sr = get_data_per_day_from_file(postal_code, column, NUM_DAYS)
     sr = sr.reindex(idx, fill_value=0)
 
-    if any(sr<0) or postal_code in ['WI','MN','NY','TN']:
-        sr = get_data_per_day_from_ctp(postal_code, column, NUM_DAYS)
-        sr = sr.reindex(idx, fill_value=0)
+    #if any(sr<0) or postal_code in ['WI','MN','NY','TN']:
+    sr = get_data_per_day_from_ctp(postal_code, column, NUM_DAYS)
+    sr = sr.reindex(idx, fill_value=0)
 
     sr = sr.dropna().astype(int)
     return sr
