43027150bb9ce4f19eb9a6e3179cd93cc3c3cb78
Kevin Nasto <kevinnasto@gmail.com>
2020-07-20 10:17:41 -0500

    Added LA to issues

--- chart.py
+++ chart.py
@@ -59,7 +59,7 @@ def get_last_n(postal_code, column):
     sr = get_data_per_day_from_file(postal_code, column, NUM_DAYS)
     sr = sr.reindex(idx, fill_value=0)
 
-    if any(sr<0) or postal_code in ['MD']:
+    if any(sr<0) or postal_code in ['MD','LA']:
         sr = get_data_per_day_from_ctp(postal_code, column, NUM_DAYS)
         sr = sr.reindex(idx, fill_value=0)
 
